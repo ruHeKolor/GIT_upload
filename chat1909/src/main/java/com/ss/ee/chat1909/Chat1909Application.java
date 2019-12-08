9cd712aa082a2fe23be07f68f8755f2a9df4cd84
ruHeKolor <37122216+ruHeKolor@users.noreply.github.com>
2019-12-09 02:22:12 +0300

    Working chat with security_basic

---
+++ chat1909/src/main/java/com/ss/ee/chat1909/Chat1909Application.java
@@ -0,0 +1,13 @@
+package com.ss.ee.chat1909;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat1909Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chat1909Application.class, args);
+	}
+
+}
